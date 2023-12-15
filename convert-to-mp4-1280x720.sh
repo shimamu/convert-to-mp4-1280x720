7b072a116065a7151225776c315ed4d117762ad5
shimamu <cooklecurry@gmail.com>
2023-12-15 10:05:21 +0900

    conversion optimization

--- convert-to-mp4-1280x720.sh
+++ convert-to-mp4-1280x720.sh
@@ -3,17 +3,30 @@
 convert_to_mp4() {
     file=$1
     codec=`ffprobe $file 2>&1 | grep "Audio:" | awk '{print $4}'`
-    if [[ "$codec" == "ac3" ]]; then
+    if [ "$codec" == "ac3" ] || [ "$codec" == "mp3" ]; then
+        echo "codec ac3 or mp3"
         ffmpeg -i $file \
             -ac 2 \
             -acodec ac3 \
-            -ab 256k \
+            -b:a 128k \
             -vcodec mpeg4 \
             -s 1280x720 \
-            -b 3000k \
+            -b:v 2000k \
             -r 24 \
             ${file}_ac3.mp4
+    elif [ "$codec" == "wmapro" ] || [ "$codec" == "wmav2" ]; then
+        echo "wmv"
+        ffmpeg -i $file \
+            -ac 2 \
+            -acodec aac \
+            -b:a 128k \
+            -vcodec mpeg4 \
+            -s 1280x720 \
+            -b:v 3000k \
+            -r 24 \
+            ${file}.mp4
     else
+        echo "codec other"
         ffmpeg -y -vsync 1 \
             -i $file \
             -f mp4 \
@@ -21,6 +34,9 @@ convert_to_mp4() {
             -map 0:0 \
             -map 0:1 \
             -s 1280x720 \
+            -r 30 \
+            -b:a 128k \
+            -b:v 1000k \
             -aspect 16:9 \
             -acodec copy \
             -bsf:a aac_adtstoasc \
@@ -31,7 +47,7 @@ convert_to_mp4() {
 if [ $# -ne 0 ]; then
     convert_to_mp4 $1
 else
-    for file in `ls -1 | egrep "(ts|mp4)$"`
+    for file in `ls -1 | egrep "(mp4|ts|wmv|mkv|avi|m4v)$"`
     do
         echo "file: $file"
         convert_to_mp4 $file
